1e400be5a49c61540e0f5835277517a037a25de7
Bryan Kizer <brykizer@gmail.com>
2018-01-31 22:28:38 -0500

    test: more benchmarks and unit tests

---
+++ source/_util/isFunction/isFunction.test.js
@@ -0,0 +1,100 @@
+import 'babel-core/register';
+import test from 'ava';
+import isFunction from './index';
+
+class TestClass {}
+
+test('returns false for object', t => {
+  const val = { foo: 'bar' };
+  const actual = isFunction(val);
+
+  t.false(actual);
+});
+
+test('returns false for class object', t => {
+  const val = new TestClass();
+  const actual = isFunction(val);
+
+  t.false(actual);
+});
+
+test('returns false for string', t => {
+  const val = 'foobar';
+  const actual = isFunction(val);
+
+  t.false(actual);
+});
+
+test('returns false for string object', t => {
+  const val = String('foobar');
+  const actual = isFunction(val);
+
+  t.false(actual);
+});
+
+test('returns false for number', t => {
+  const val = 42;
+  const actual = isFunction(val);
+
+  t.false(actual);
+});
+
+test('returns false for number object', t => {
+  const val = Number(42);
+  const actual = isFunction(val);
+
+  t.false(actual);
+});
+
+test('returns false for boolean', t => {
+  const val = true;
+  const actual = isFunction(val);
+
+  t.false(actual);
+});
+
+test('returns false for null', t => {
+  const val = null;
+  const actual = isFunction(val);
+
+  t.false(actual);
+});
+
+test('returns false for undefined', t => {
+  const val = undefined;
+  const actual = isFunction(val);
+
+  t.false(actual);
+});
+
+test('returns true for function', t => {
+  const val = () => {};
+  const actual = isFunction(val);
+
+  t.true(actual);
+});
+
+test('returns false for array', t => {
+  const val = [ 1, 2, 3 ];
+  const actual = isFunction(val);
+
+  t.false(actual);
+});
+
+/*
+┌────────────────────────────────────────┬────────────────────┬────────────────────┐
+│ isFunction                             │ Hertz              │ Margin of Error    │
+├────────────────────────────────────────┼────────────────────┼────────────────────┤
+│ typeof                                 │ 861,797,073        │ 0.19%              │
+├────────────────────────────────────────┼────────────────────┼────────────────────┤
+│ constructor                            │ 859,013,504        │ 0.20%              │
+├────────────────────────────────────────┼────────────────────┼────────────────────┤
+│ constructor 2                          │ 861,257,297        │ 0.17%              │
+├────────────────────────────────────────┼────────────────────┼────────────────────┤
+│ instanceof                             │ 194,121,617        │ 0.27%              │
+├────────────────────────────────────────┼────────────────────┼────────────────────┤
+│ prototype.toString                     │ 160,487,648        │ 0.30%              │
+├────────────────────────────────────────┼────────────────────┼────────────────────┤
+│ isFunction                             │ 859,414,625        │ 0.22%              │
+└────────────────────────────────────────┴────────────────────┴────────────────────┘
+ */
--- source/_util/isObject/isObject.bench.js
+++ source/_util/isObject/isObject.bench.js
@@ -7,23 +7,27 @@ const data = { foo: 'bar', bench: 'mark', functional: 'programming' };
 module.exports = {
   name: 'isObject',
   tests: {
-    ['val & typeof & constructor']() {
+    ['val & typeof & constructor (no class)']() {
       return data && typeof data === 'object' && data.constructor === Object;
     },
-    ['val & constructor *']() {
+    ['val & constructor (no class)']() {
       return data && data.constructor === Object;
     },
-    ['val & typeof']() {
+    ['val & typeof (no class)']() {
       return data && typeof value === 'object'; // Auto fails since it picks up other types
     },
-    ['prototype.toString']() {
+    ['prototype.toString (rambda)']() {
       return Object.prototype.toString.call(data) === '[object Object]';
     },
-    ['obj === Object']() {
+    ['obj === Object (no class)']() {
       return data === Object(data); // Auto fails since it picks up other types
     },
-    ['instanceof & constructor']() {
+    ['instanceof & constructor (no class)']() {
       return data instanceof Object && data.constructor === Object;
+    },
+    ['lodash (includes array & func)']() {
+      const type = typeof data;
+      return data != null && (type === 'object' || type === 'function');
     },
     ['isObject']() {
       return isObject(data);
@@ -32,22 +36,23 @@ module.exports = {
 };
 
 /*
-┌───────────────────────────────────┬────────────────────┬────────────────────┐
-│ isObject                          │ Hertz              │ Margin of Error    │
-├───────────────────────────────────┼────────────────────┼────────────────────┤
-│ val & typeof & constructor        │ 857,718,438        │ 0.17%              │
-├───────────────────────────────────┼────────────────────┼────────────────────┤
-│ val & constructor                 │ 859,628,821        │ 0.14%              │
-├───────────────────────────────────┼────────────────────┼────────────────────┤
-│ val & typeof                      │ 132,781,332        │ 0.39%              │
-├───────────────────────────────────┼────────────────────┼────────────────────┤
-│ prototype.toString                │ 185,928,696        │ 0.41%              │
-├───────────────────────────────────┼────────────────────┼────────────────────┤
-│ obj === Object                    │ 143,103,430        │ 0.20%              │
-├───────────────────────────────────┼────────────────────┼────────────────────┤
-│ instanceof & constructor *        │ 860,960,541        │ 0.12%              │
-├───────────────────────────────────┼────────────────────┼────────────────────┤
-│ isObject *                        │ 860,040,121        │ 0.12%              │
-└───────────────────────────────────┴────────────────────┴────────────────────┘
-  Fastest is instanceof & constructor,isObject
+┌────────────────────────────────────────┬────────────────────┬────────────────────┐
+│ isObject                               │ Hertz              │ Margin of Error    │
+├────────────────────────────────────────┼────────────────────┼────────────────────┤
+│ val & typeof & constructor (no class)  │ 861,778,373        │ 0.19%              │
+├────────────────────────────────────────┼────────────────────┼────────────────────┤
+│ val & constructor (no class)           │ 863,181,655        │ 0.19%              │
+├────────────────────────────────────────┼────────────────────┼────────────────────┤
+│ val & typeof (no class)                │ 135,546,796        │ 0.30%              │
+├────────────────────────────────────────┼────────────────────┼────────────────────┤
+│ prototype.toString (rambda)            │ 187,472,138        │ 0.34%              │
+├────────────────────────────────────────┼────────────────────┼────────────────────┤
+│ obj === Object (no class)              │ 143,007,606        │ 0.30%              │
+├────────────────────────────────────────┼────────────────────┼────────────────────┤
+│ instanceof & constructor (no class)    │ 862,365,786        │ 0.14%              │
+├────────────────────────────────────────┼────────────────────┼────────────────────┤
+│ lodash (includes array & func)         │ 863,629,474        │ 0.12%              │
+├────────────────────────────────────────┼────────────────────┼────────────────────┤
+│ isObject                               │ 862,942,577        │ 0.14%              │
+└────────────────────────────────────────┴────────────────────┴────────────────────┘
  */
