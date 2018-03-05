fc4a95bd0bbb236dc16ef8a64a40566dc5dd7234
Bryan Kizer <brykizer@gmail.com>
2018-03-05 01:32:01 -0500

    build: changed bench runner to check against implemented

--- benchRunner.js
+++ benchRunner.js
@@ -3,30 +3,27 @@ const glob = require('glob');
 const Benchmark = require('benchmark');
 const Table = require('cli-table');
 
-const benchGlob = 'source/**/*.bench.js';
+const BENCHES = 'source/**/*.bench.js';
+const THRESHOLD = 0.15;
 
-function blue(str) {
-  return `\x1b[94m${ str }\x1b[0m`;
-}
+const blue = str => `\x1b[94m${ str }\x1b[0m`;
 
-function green(str) {
-  return `\x1b[92m${ str }\x1b[0m`;
-}
+const green = str => `\x1b[92m${ str }\x1b[0m`;
 
-function getTable(name) {
-  return new Table({
-    head: [ name, 'Hertz', 'Margin of Error' ],
+const getTable = name =>
+  new Table({
+    head: [ name, 'Hertz', 'Count' ],
     colWidths: [ 40, 20, 20 ],
   });
-}
 
-function prettyHz(hz) {
-  return Benchmark.formatNumber(hz.toFixed(hz < 100 ? 2 : 0));
-}
+const prettyHz = hz =>
+  Benchmark.formatNumber(hz.toFixed(hz < 100 ? 2 : 0));
 
-function prettyMoe(moe) {
-  return `${ moe.toFixed(2) }%`;
-}
+const prettyNum = num => `${ num.toLocaleString('en') }`;
+
+const benchNames = s => Object.keys(s).filter(x => !isNaN(x)).sort();
+
+const difference = (a, b) => (a - b) / a;
 
 function runBenchmarks(files) {
   files.forEach(file => {
@@ -41,20 +38,31 @@ function runBenchmarks(files) {
     Object.keys(test.tests).forEach(k => {
       suite.add(k, test.tests[k], {
         onComplete(vo) {
-          table.push([ vo.target.name, prettyHz(vo.target.hz), prettyMoe(vo.target.stats.rme) ]);
+          table.push([ vo.target.name, prettyHz(vo.target.hz), prettyNum(vo.target.count) ]);
         },
       });
     });
 
     suite
       .on('complete', () => {
-        console.log(`  ${ blue('Fastest is ') }${ green(suite.filter('fastest').map('name')) }`);
+        const benches = benchNames(suite).map(x => suite[x]);
+        const fastest = benches.reduce((acc, curr) =>
+          curr.count > (acc.count || 0) ? curr : acc, {});
+        const fjp = benches.filter(x => x.name.startsWith('fjp.'))[0];
+        const diff = difference(fastest.count, fjp.count);
+
+        if(diff > THRESHOLD) {
+          const msg = `Implemented is not within ${ THRESHOLD * 100 }% of fastest: [${ fastest.count }, ${ fjp.count }]`;
+
+          throw new RangeError(msg);
+        }
+
+        console.log(`  ${ blue('Fastest: ') }${ green(fastest.name) }${ blue(' @ ') }${ green(prettyNum(fastest.count)) }`);
+        console.log(`  ${ blue('FJP Diff Compared to Fastest: ') }${ green((diff * 100).toFixed(4)) }%`);
         console.log();
       })
       .run({ async: true });
   });
 }
 
-glob(benchGlob, (error, files) => {
-  runBenchmarks(files);
-});
+glob(BENCHES, (error, files) => runBenchmarks(files));
