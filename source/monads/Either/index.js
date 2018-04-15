b2460bc23b2b51971125753f776238cdf117707e
Bryan Kizer <brykizer@gmail.com>
2018-04-15 17:34:25 -0400

    feat: Either monad

---
+++ source/monads/Either/index.js
@@ -0,0 +1,112 @@
+class Either {
+  static of(x) {
+    // eslint-disable-next-line no-use-before-define
+    return x !== undefined && x !== null ? new Right(x) : new Left(x);
+  }
+
+  static fromNullable(x) {
+    return Either.of(x);
+  }
+
+  static Right(x) {
+    // eslint-disable-next-line no-use-before-define
+    return new Right(x);
+  }
+
+  static Left(x) {
+    // eslint-disable-next-line no-use-before-define
+    return new Left(x);
+  }
+
+  get isRight() {
+    return false;
+  }
+
+  get isLeft() {
+    return false;
+  }
+
+  map() {
+    return this;
+  }
+
+  filter() {
+    return this;
+  }
+
+  chain() {
+    return this;
+  }
+
+  cata(leftFn, rightFn) {
+    return this.isRight ? rightFn(this._val) : leftFn(this._val);
+  }
+
+  orElseFn() {
+    return this;
+  }
+}
+
+class Right extends Either {
+  constructor(val) {
+    super();
+    this._val = val;
+  }
+
+  get value() {
+    return this._val;
+  }
+
+  get isRight() {
+    return true;
+  }
+
+  map(fn) {
+    return Either.of(fn(this._val));
+  }
+
+  filter(fn) {
+    Either.of(fn(this._val) ? this._val : null);
+  }
+
+  orElse() {
+    return this._val;
+  }
+
+  chain(fn) {
+    return fn(this._val);
+  }
+
+  toString() {
+    return `Either.Right(${ this.value })`;
+  }
+}
+
+class Left extends Either {
+  constructor(val) {
+    super();
+    this._val = val;
+  }
+
+  get value() {
+    throw new TypeError("Can't extract the value of a Left");
+  }
+
+  get isLeft() {
+    return true;
+  }
+
+  orElse(other) {
+    return other;
+  }
+
+  orElseFn(fn) {
+    return fn(this._val);
+  }
+
+  toString() {
+    return `Either.Left(${ this._val })`;
+  }
+}
+
+export default Either;
