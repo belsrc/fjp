ed28da69f0324bece91ee75f8683d4b88b4feff4
Bryan Kizer <brykizer@gmail.com>
2018-04-15 18:42:23 -0400

    refactor: simplified class struct, added quality of life

--- source/monads/Maybe/index.js
+++ source/monads/Maybe/index.js
@@ -1,8 +1,17 @@
 class Maybe {
   static of(x) {
-    /* eslint-disable no-use-before-define */
+    // eslint-disable-next-line no-use-before-define
     return x !== undefined && x !== null ? new Just(x) : new Nothing();
-    /* eslint-enable no-use-before-define */
+  }
+
+  static Just(x) {
+    // eslint-disable-next-line no-use-before-define
+    return new Just(x);
+  }
+
+  static None() {
+    // eslint-disable-next-line no-use-before-define
+    return new Nothing();
   }
 
   static fromNullable(x) {
@@ -20,6 +29,26 @@ class Maybe {
   get isJust() {
     return false;
   }
+
+  flatten() {
+    return this;
+  }
+
+  join() {
+    return this;
+  }
+
+  filter() {
+    return this;
+  }
+
+  map() {
+    return this;
+  }
+
+  chain(fn) {
+    return this.map(fn).flatten();
+  }
 }
 
 class Just extends Maybe {
@@ -32,58 +61,54 @@ class Just extends Maybe {
     return this._val;
   }
 
-  map(fn) {
-    return Maybe.of(fn(this.value));
-  }
-
-  join() {
-    if(!this.value instanceof Just) {
-      return this;
-    }
-
-    return this.value.join();
-  }
-
-  orElse() {
-    return this.value;
-  }
-
-  filter(fn) {
-    Maybe.fromNullable(fn(this.value) ? this.value : null);
-  }
-
   get isJust() {
     return true;
   }
 
+  map(fn) {
+    return Maybe.of(fn(this._val));
+  }
+
+  flatten() {
+    if(!(this._val instanceof Just)) {
+      return this;
+    }
+
+    return this._val.flatten();
+  }
+
+  join() {
+    return this.flatten();
+  }
+
+  orElse() {
+    return this._val;
+  }
+
+  filter(fn) {
+    Maybe.of(fn(this._val) ? this._val : null);
+  }
+
   toString() {
-    return `Maybe.Just(${ this.value })`;
+    return `Maybe.Just(${ this._val })`;
   }
 }
 
 class Nothing extends Maybe {
-  map() {
-    return this;
+  constructor() {
+    super();
   }
 
   get value() {
-    throw new TypeError("Can' extract the value of a Nothing");
-  }
-
-  join() {
-    return this;
-  }
-
-  orElse(other) {
-    return other;
-  }
-
-  filter() {
-    return this.value;
+    throw new TypeError("Can't extract the value of a Nothing");
   }
 
   get isNothing() {
     return true;
+  }
+
+  orElse(other) {
+    return other;
   }
 
   toString() {
