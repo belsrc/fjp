e1938691e0f9ac42198df85954c92b402ae63d46
Bryan Kizer <brykizer@gmail.com>
2022-04-21 09:36:42 -0400

    build: update rollup config for new packages

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
-import babel from 'rollup-plugin-babel';
-import resolve from 'rollup-plugin-node-resolve';
-import babili from 'rollup-plugin-babili';
+import { babel } from '@rollup/plugin-babel';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { terser } from "rollup-plugin-terser";
 const pkg = require('./package.json');
 
 const config = {
@@ -10,11 +10,11 @@ const config = {
     exports: 'named',
     name: pkg.name,
   },
-  plugins: [ resolve(), babel({ exclude: 'node_modules/**' }) ],
+  plugins: [ nodeResolve(), babel({ babelHelpers: 'bundled', exclude: 'node_modules/**' }) ],
 };
 
 if(process.env.NODE_ENV === 'production') {
-  config.plugins.push(babili());
+  config.plugins.push(terser());
 }
 
 module.exports = config;
